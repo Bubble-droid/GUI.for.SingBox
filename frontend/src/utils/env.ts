570b4c560dbc3d2c9a63103603eb4e01545a0b14
Bubble-droid <169905840+Bubble-droid@users.noreply.github.com>
2026-07-23 13:51:12 +0800

    build: update build environment configs

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -1,3 +1,5 @@
+export const APP_IDENTIFIER = import.meta.env.VITE_APP_IDENTIFIER
+
 export const APP_TITLE = import.meta.env.VITE_APP_TITLE
 
 export const APP_VERSION = import.meta.env.VITE_APP_VERSION
